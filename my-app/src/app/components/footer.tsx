3df7120fc01e8a01716ba8d6443fa454ea371f43
Misho <chapidze.misho95@gmail.com>
2024-06-12 18:26:28 +0400

    fxdd

--- my-app/src/app/components/footer.tsx
+++ my-app/src/app/components/footer.tsx
@@ -2,11 +2,16 @@ import Image from "next/image";
 import Link from "next/link";
 import { navigation } from "./header/header";
 import FaceBookSVG from "/public/shared/desktop/facebook.svg";
+import YouTubeSVG from "/public/shared/desktop/youtube.svg";
+import TwitterSVG from "/public/shared/desktop/twitter.svg";
+import PinterestSVG from "/public/shared/desktop/pinterest.svg";
+import InstagramSVG from "/public/shared/desktop/instagram.svg";
+import ArrowButton from "./arrow-button";
 
 const Footer = () => {
   return (
-    <footer className="w-full flex items-center justify-between h-[250px] bg-black text-white px-[171px] py-[64px] relative">
-      <div className="grid grid-cols-2 grid-rows-2 gap-[85px_100px]">
+    <footer className="w-full flex flex-col sm:flex-row items-center justify-between gap-10 sm:h-[284px] lg:h-[250px] bg-black text-white px-[33px] sm:px-[171px] py-[56px] sm:py-[64px] relative">
+      <div className="grid grid-cols-1 lg:grid-cols-2 lg:grid-rows-2 gap-[50px_121px] lg:gap-[85px_100px]">
         <Link href="/">
           <Image
             src={"/logo-white.svg"}
@@ -16,7 +21,7 @@ const Footer = () => {
             className="w-[170px] h-[16px]"
           />
         </Link>
-        <nav className="row-span-2 uppercase flex flex-col gap-[19px] text-[12px] font-bold leading-[2px]">
+        <nav className="lg:row-span-2 uppercase flex flex-col sm:flex-row lg:flex-col gap-[35px] text-[12px] font-bold leading-[2px] items-center">
           <Link href="/" className=" active:opacity-30">
             home
           </Link>
@@ -28,9 +33,19 @@ const Footer = () => {
             );
           })}
         </nav>
-        <div>
-          <FaceBookSVG />
+        <div className="flex gap-4">
+          <FaceBookSVG className="fill-white hover:fill-[#63AFDB]" />
+          <YouTubeSVG className="fill-white hover:fill-[#D3205A]" />
+          <TwitterSVG className="fill-white hover:fill-[#65FFEB]" />
+          <PinterestSVG className="fill-white hover:fill-[#F6C683]" />
+          <InstagramSVG className="fill-white hover:fill-[#BC7198]" />
         </div>
+      </div>
+      <div className="h-full flex flex-col justify-between gap-5">
+        <ArrowButton variant={"light"} title={"GET AN INVITE"} />
+        <p className="text-[15px] opacity-50">
+          Copyright 2019. All Rights Reserved
+        </p>
       </div>
     </footer>
   );
